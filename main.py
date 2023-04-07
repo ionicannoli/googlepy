2062b6a789e2ad0480007ba60afcfdb46b51bcc6
unknown <ionascu.teodoralexandru@gmail.com>
2023-04-07 18:30:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#nrmere=2
+#nrpere=3
+#mesaj=f"ana are {nrmere} mere si {nrpere} pere"
+#mesaj="ana are"+str(nrmere)+"mere si "+str(nrpere)+" pere"
+#mesaj=f'ana are"{nrmere}"mere si"{nrpere}" pere'
+#print(mesaj)
+
+#lista=[8,-2,'e',3.1,"ana",None]
+#print(type(lista[0])
+#print(lista.index[0])
+#print(lista[0])
+#print(lista[0:8])
+#print(lista[6:7])
+#print(lista[:4])
+#print(lista[0:3:1])
+#lista.append(5)
+#lista[2]=4
+#print(lista)
+
+#tuplu=(1,7,-2)
+#print(type(tuplu))
+
+#dictionar={"key1":1,
+           #"key2":2,
+           #3:4,
+           #5.3:[0,2,5]}
+#print(dictionar, type(dictionar))
+
+a=2
+b=2
+c=3
+if a<b:
+    print("a mai mic ca b")
+elif a>b:
+    print("a mai mare ca b")
+elif a<c:
+    print("a mai mic ca c")
+else:
+    print("a=b")
