781149c268a83f6ec83a457e4e8b098eb6179427
ionicannoli <129886428+ionicannoli@users.noreply.github.com>
2023-05-16 19:12:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,67 @@
-#nrmere=2
-#nrpere=3
-#mesaj=f"ana are {nrmere} mere si {nrpere} pere"
-#mesaj="ana are"+str(nrmere)+"mere si "+str(nrpere)+" pere"
-#mesaj=f'ana are"{nrmere}"mere si"{nrpere}" pere'
-#print(mesaj)
+class Catalog:
+    def __init__(self, nume, prenume):
+        self.nume = nume
+        self.prenume = prenume
+        self.materii = {}
+        self.absente = 0
 
-#lista=[8,-2,'e',3.1,"ana",None]
-#print(type(lista[0])
-#print(lista.index[0])
-#print(lista[0])
-#print(lista[0:8])
-#print(lista[6:7])
-#print(lista[:4])
-#print(lista[0:3:1])
-#lista.append(5)
-#lista[2]=4
-#print(lista)
+    def __str__(self):
+        return f"Nume: {self.nume}\nPrenume: {self.prenume}\nAbsente: {self.absente}"
 
-#tuplu=(1,7,-2)
-#print(type(tuplu))
+    def incrementare_absente(self, nr_absente=1):
+        self.absente += nr_absente
 
-#dictionar={"key1":1,
-           #"key2":2,
-           #3:4,
-           #5.3:[0,2,5]}
-#print(dictionar, type(dictionar))
+    def stergere_absente(self, nr_absente):
+        if self.absente >= nr_absente:
+            self.absente -= nr_absente
 
-a=2
-b=2
-c=3
-if a<b:
-    print("a mai mic ca b")
-elif a>b:
-    print("a mai mare ca b")
-elif a<c:
-    print("a mai mic ca c")
-else:
-    print("a=b")
+class Extensie1(Catalog):
+    def adauga_materie(self, materia, note):
+        self.materii[materia] = note
+
+    def afisare_materii(self):
+        for materia in self.materii:
+            print(materia)
+
+    def afisare_medii(self):
+        for materia, note in self.materii.items():
+            suma_note = 0
+            numar_note = 0
+            for nota in note:
+                if isinstance(nota, (int, float)):
+                    suma_note += nota
+                    numar_note += 1
+            if numar_note > 0:
+                media = suma_note / numar_note
+                print(f"Materia: {materia}, Media: {media}")
+
+
+# Crearea studentilor si efectuarea operatiilor specifice
+student1 = Extensie1("Ion", "Roata")
+student1.incrementare_absente(3)
+student1.stergere_absente(2)
+
+student2 = Extensie1("George", "Cerc")
+student2.incrementare_absente(4)
+student2.stergere_absente(2)
+
+print(f"Absentele studentului {student1.nume} {student1.prenume}: {student1.absente}")
+print(f"Absentele studentului {student2.nume} {student2.prenume}: {student2.absente}")
+
+student1.adauga_materie("Python", [8, 9, 7])
+student1.adauga_materie("Romana", [9, 9, 10])
+
+student2.adauga_materie("Python", [7, 6, 8])
+student2.adauga_materie("Matematica", [9, 8, 9])
+
+print("Materiile studentului Ion Roata:")
+student1.afisare_materii()
+
+print("Materiile studentului George Cerc:")
+student2.afisare_materii()
+
+print("Mediile studentului Ion Roata:")
+student1.afisare_medii()
+
+print("Mediile studentului George Cerc:")
+student2.afisare_medii()
